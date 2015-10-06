d11bebc32c6e405655e1168c665581928395d5cb
DominikTher <dominik.ther@windowslive.com>
2015-10-06 13:44:36 +0200

    Some other functions added

--- BIA_Functions/TestFunctions.cs
+++ BIA_Functions/TestFunctions.cs
@@ -43,46 +43,46 @@ namespace BIA_Functions
             return value;
         }
 
-        //[FunctionName("De jongg")]
-        //public double DeJongNo4(double[] x)
-        //{
-        //    var value = 0.0;
+        [FunctionName("4th De Jong")] // TODO: Test with another student if the graph result is same
+        public double DeJongNo4(double[] x)
+        {
+            var value = 0.0;
 
-        //    for (int i = 0; i < x.Length; i++)
-        //    {
-        //        value += (i * Math.Pow(x[i], 4));
-        //    }
+            for (int i = 0; i < x.Length; i++)
+            {
+                value += (i * Math.Pow(x[i], 4));
+            }
 
-        //    return value;
-        //}
+            return value;
+        }
 
-        //[FunctionName("De jofng")]
-        //public double Rastrigins(double[] x)
-        //{
-        //    var value = 0.0;
+        [FunctionName("Rastrigin’s function")]
+        public double Rastrigins(double[] x)
+        {
+            var value = 0.0;
 
-        //    for (int i = 0; i < x.Length; i++)
-        //    {
-        //        value += (Math.Pow(x[i], 2) - 10 * Math.Cos(2 * Math.PI * x[i]));
-        //    }
+            for (int i = 0; i < x.Length; i++)
+            {
+                value += (Math.Pow(x[i], 2) - 10 * Math.Cos(2 * Math.PI * x[i]));
+            }
 
-        //    value *= (2 * x.Length); // TODO: Test it and test 10cos vs 10 * cos
+            value *= (2 * x.Length); // TODO: Test it and test 10cos vs 10 * cos
 
-        //    return value;
-        //}
+            return value;
+        }
 
-        //[FunctionName("Dffe jong")]
-        //public double Schwefels(double[] x)
-        //{
-        //    var value = 0.0;
+        [FunctionName("Schwefel’s function")]
+        public double Schwefels(double[] x)
+        {
+            var value = 0.0;
 
-        //    for (int i = 0; i < x.Length; i++)
-        //    {
-        //        value += ((-1 * x[i]) * Math.Sin(Math.Sqrt(Math.Abs(x[i]))));
-        //    }
+            for (int i = 0; i < x.Length; i++)
+            {
+                value += ((-1 * x[i]) * Math.Sin(Math.Sqrt(Math.Abs(x[i]))));
+            }
 
-        //    return value;
-        //}
+            return value;
+        }
 
         //public double Griewangks(double[] x,)
         //{
@@ -98,32 +98,47 @@ namespace BIA_Functions
         //    return value;
         //}
 
-        //[FunctionName("De fgjong")]
-        //public double SineEnvelopeSineWave(double[] x)
-        //{
-        //    var value = 0.0;
+        [FunctionName("Sine envelope sine wave function")] // TODO: Test with another student
+        public double SineEnvelopeSineWave(double[] x)
+        {
+            var value = 0.0;
 
-        //    for (int i = 0; i < x.Length - 1; i++)
-        //    {
-        //        value += (0.5 + (
-        //            (Math.Pow(
-        //                Math.Pow(x[i], 2) +
-        //                Math.Pow(x[i + 1], 2) -
-        //                0.5
-        //                , 2))
-        //                /
-        //                (1 + (
-        //                0.001 * Math.Pow(
-        //                Math.Pow(x[i], 2) +
-        //                Math.Pow(x[i + 1], 2)
-        //                , 2)
-        //                )))
-        //            );
-        //    }
+            for (int i = 0; i < x.Length - 1; i++)
+            {
+                value += (0.5 + (
+                    (Math.Pow(
+                        Math.Pow(x[i], 2) +
+                        Math.Pow(x[i + 1], 2) -
+                        0.5
+                        , 2))
+                        /
+                        (1 + (
+                        0.001 * Math.Pow(
+                        Math.Pow(x[i], 2) +
+                        Math.Pow(x[i + 1], 2)
+                        , 2)
+                        )))
+                    );
+            }
 
-        //    value *= -1;
+            value *= -1;
 
-        //    return value;
-        //}
+            return value;
+        }
+
+        [FunctionName("Stretched V sine wave function")]
+        public double StretchedVSineWave(double[] x)
+        {
+            var value = 0.0;
+
+            for (int i = 0; i < x.Length - 1; i++)
+            {
+                var tmp = Math.Pow(x[i], 2) + Math.Pow(x[i + 1], 2);
+                value += (Math.Pow(tmp, 1.0 / 4.0) *
+                            Math.Pow(Math.Sin(50 * Math.Pow(tmp, 1.0 / 10.0)), 2) + 1);
+            }
+
+            return value;
+        }
     }
 }
