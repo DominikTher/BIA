da689a2ea3329bde45614740579190cd82237afd
DominikTher <dominik.ther@windowslive.com>
2015-12-01 21:19:05 +0100

    Some new functions

---
+++ BIA_Functions/ES.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BIA_Functions
+{
+    internal class ES
+    {
+        private int iteration;
+        private double delta;
+        private double FV;
+
+        public void Step()
+        {
+            if (iteration-- > 0)
+            {
+            }
+        }
+    }
+}
